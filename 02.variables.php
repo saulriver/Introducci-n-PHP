4ab550237ebefc90a8853a3529034fbf06846dd8
saul <sarima666@gmail.com>
2020-05-08 10:04:53 -0500

    Update 02.variables.php

--- 02.variables.php
+++ 02.variables.php
@@ -3,7 +3,7 @@
 #Variable numerica
 $numero = 5;
 echo "Esto es una variable numero: $numero <br>";
-#metodo var_dump() me muestra que tipo de variable estoy usando
+#metodo var_dump() me muestra que tipo de variable estoy usando y sirve para identificar errores de las variables que estamos llamando
 var_dump($numero);
 #tambuen se puede crear el salto de linea asi:
 echo "<br><br>";
@@ -11,7 +11,7 @@ echo "<br><br>";
 #Variable texto
 $palabra = "Palabra";
 echo "Esto es una variable texto: $palabra <br>";
-#metodo var_dump() me muestra que tipo de variable estoy usando
+#metodo var_dump() me muestra que tipo de variable estoy usando y sirve para identificar errores de las variables que estamos llamando
 var_dump($palabra);
 #tambuen se puede crear el salto de linea asi:
 echo "<br><br>";
@@ -21,7 +21,7 @@ echo "<br><br>";
 $boleana1 = true;
 $boleana = false;
 echo "Esto es una variable boleana true: $boleana1 cuando son true muestra 1. <br>";
-#metodo var_dump() me muestra que tipo de variable estoy usando
+#metodo var_dump() me muestra que tipo de variable estoy usando y sirve para identificar errores de las variables que estamos llamando
 var_dump($boleana1);
 #tambien se puede crear el salto de linea asi:
 echo "<br><br>";
@@ -33,7 +33,7 @@ echo "<br><br>";
 #Variable arreglo
 $colores = array("Rojo", "Amarillo");
 echo "Esto es una variable arreglo: $colores[0] <br>";
-#metodo var_dump() me muestra que tipo de variable estoy usando
+#metodo var_dump() me muestra que tipo de variable estoy usando y sirve para identificar errores de las variables que estamos llamando
 var_dump($colores);
 #tambuen se puede crear el salto de linea asi:
 echo "<br><br>";
@@ -41,7 +41,7 @@ echo "<br><br>";
 #Variable arreglo con propiedades
 $verduras = array("Verdura1" => "Lechuga", "Verdura2" => "Cebolla");
 echo "Esto es una variable arreglo con propiedades: $verduras[Verdura1] <br>";
-#metodo var_dump() me muestra que tipo de variable estoy usando
+#metodo var_dump() me muestra que tipo de variable estoy usando y sirve para identificar errores de las variables que estamos llamando
 var_dump($verduras);
 #tambuen se puede crear el salto de linea asi:
 echo "<br><br>";
@@ -49,7 +49,7 @@ echo "<br><br>";
 #Variable objeto
 $frutas = (object)["fruta1" => "Manzana", "fruta2" => "Pera"];
 echo "Esto es una variable objeto: $frutas->fruta2 <br>";
-#metodo var_dump() me muestra que tipo de variable estoy usando
+#metodo var_dump() me muestra que tipo de variable estoy usando y sirve para identificar errores de las variables que estamos llamando
 var_dump($frutas);
 #tambuen se puede crear el salto de linea asi:
 echo "<br><br>";
