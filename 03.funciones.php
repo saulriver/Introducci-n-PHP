50c8558bc24761e60a77c9c88a6dec83e2c471a6
saul <sarima666@gmail.com>
2020-05-08 10:15:50 -0500

    Update 03.funciones.php

--- 03.funciones.php
+++ 03.funciones.php
@@ -1,9 +1,14 @@
 <?php
 #funciones sin parametros
 function saludo(){
-    echo "Hola";
+    echo "Hola <br>";
 }
 saludo();
 
+#funciones con parametros
+function despedida($adios){
+    echo $adios."<br>";
+}
+despedida("Chao lola");
 
 ?>
