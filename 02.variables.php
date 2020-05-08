1c1ea0e20114d844ac006267f11d2fa7d83aceae
saul <sarima666@gmail.com>
2020-05-07 23:54:08 -0500

    Update 02.variables.php

--- 02.variables.php
+++ 02.variables.php
@@ -25,19 +25,19 @@ echo "<br><br>";
 #Variable arreglo
 $colores = array("Rojo", "Amarillo");
 echo "Esto es una variable arreglo: $colores[0]";
-#tambuen se puede crear el salto de linea asi:
+#tambien se puede crear el salto de linea asi:
 echo "<br><br>";
 
 #Variable arreglo con propiedades
 $verduras = array("Verdura1" => "Lechuga", "Verdura2" => "Cebolla");
 echo "Esto es una variable arreglo con propiedades: $verduras[Verdura1]";
-#tambuen se puede crear el salto de linea asi:
+#tambien se puede crear el salto de linea asi:
 echo "<br><br>";
 
 #Variable objeto
 $frutas = (object)["fruta1" => "Manzana", "fruta2" => "Pera"];
 echo "Esto es una variable objeto: $frutas->fruta2";
-#tambuen se puede crear el salto de linea asi:
+#tambien se puede crear el salto de linea asi:
 echo "<br><br>";
 
 ?>
