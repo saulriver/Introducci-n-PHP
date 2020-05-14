bef96c04d188c0e428c7fb59440e4a8d8e102a6b
saul <sarima666@gmail.com>
2020-05-14 00:39:31 -0500

    vista html

--- 03.MVC/index.php
+++ 03.MVC/index.php
@@ -1,4 +1,11 @@
 <?php
 
+#El index en el mostraremos la salida de las vistas al usuario y tambien
+#a traves de el enviaremos las distintas acciones que el usuario envie al controlador.
+
+require_once "controllers/controller.php";
+
+$mvc = new MvcController();
+$mvc->plantilla();
 
 ?>
--- 03.MVC/views/template.php
+++ 03.MVC/views/template.php
@@ -10,24 +10,37 @@
     </head>
 <body>
 
-    <div class="container">
-
-        <div class="jumbotron">
-            <h1>My First Bootstrap Page</h1>
+        <div class="jumbotron" style="background-color: #263859;">
+            <h1 style="text-align:center; color: white;">PHP MVC</h1>
         </div>
+
+    <div class="container">
+        <nav class="navbar navbar-inverse">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#">MVC con PHP</a>
+                </div>
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Home</a></li>
+                    <li><a href="#">Page 1</a></li>
+                    <li><a href="#">Page 2</a></li>
+                    <li><a href="#">Page 3</a></li>
+                </ul>
+            </div>
+        </nav>
 
         <div class="row">
+            <div class="col-sm-4">
+            ...
+            </div>
                 <div class="col-sm-4">
                 ...
                 </div>
-                    <div class="col-sm-4">
-                    ...
-                    </div>
-                <div class="col-sm-4">
-                ...
-                </div>
+            <div class="col-sm-4">
+            ...
+            </div>
         </div>
-
+        
     </div>
 
 </body>
